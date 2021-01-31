b11b51edafd89a75393e3d4227512ea96a68e882
Matteo Graizzaro <graizzaromatteo@gmail.com>
2021-01-31 15:07:18 +0100

    Update MyGradient.cs
    Added class description

--- Base/MyGradient.cs
+++ Base/MyGradient.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+using UnityEngine;
 
+/// <summary>
+/// Class used to store the palette of a monster.
+/// </summary>
 public class MyGradient : MonoBehaviour {
 
     public Gradient _gradient = null;
