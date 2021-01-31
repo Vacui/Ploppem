360df841f1de1d9c1ba656552c1380cd3fc370f6
Matteo Graizzaro <graizzaromatteo@gmail.com>
2021-01-31 18:06:56 +0100

    Update ForceAction.cs
    Added class description
    Polished code

--- Utils/ForceAction.cs
+++ Utils/ForceAction.cs
@@ -1,19 +1,23 @@
-﻿using UnityEngine.UI;
+using UnityEngine.UI;
 using UnityEngine;
 
+/// <summary>
+/// Class used by UI elements to perform special actions, such as call a URL in the browser or modify a boolean value in an Animator component.
+/// </summary>
 public class ForceAction : MonoBehaviour {
 
-    public void OnClick() {
+    public void ForceBtnClick() {
         Button myButton = GetComponent<Button>();
         if (myButton != null) {
             myButton.onClick?.Invoke();
         }
     }
 
-    public void Destroy() {
+    public void DestroyObject() {
         Destroy(gameObject);
     }
 
+    // The presence of the methods SetAnimatorBoolTrue and False is necessary to overcome the interface limitation in the objects inspector for UnityEvents.
     public void SetAnimatorBoolTrue(string parameterName) {
         SetAnimatorBool(parameterName, true);
     }
