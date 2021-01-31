3b3fc0becebc5ab2e00ca27ed16f3925f48f94f1
Matteo Graizzaro <graizzaromatteo@gmail.com>
2021-01-31 17:29:49 +0100

    Update and rename Utility/Utility.cs to Utils/GradientUtils.cs
    Moved direction related methods to DirectionUtils.cs
    Polished code

---
+++ Utils/GradientUtils.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public static class GradientUtils {
+
+    public static Gradient GenerateGradient(Color color1, Color color2, float alpha1 = 1, float alpha2 = 1) {
+        Gradient result = new Gradient();
+        result.SetKeys(
+            new GradientColorKey[2] {
+                new GradientColorKey(color1, 0),
+                new GradientColorKey(color2, 1)
+            },
+            new GradientAlphaKey[2]{
+                new GradientAlphaKey(alpha1,0),
+                new GradientAlphaKey(alpha2,1)
+            });
+        return result;
+    }
+
+}
