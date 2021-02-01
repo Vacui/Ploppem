a64bb966718160859fee640071279902620a8ae5
Matteo Graizzaro <graizzaromatteo@gmail.com>
2021-02-01 16:56:23 +0100

    Add CustomUnityEvents.cs

---
+++ Utils/CustomUnityEvents.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.Events;
+
+/// <summary>
+/// Custom UnityEvent with float parameter.
+/// </summary>
+[System.Serializable] public class UnityFloatEvent : UnityEvent<float> { }
+
+/// <summary>
+/// Custom UnityEvent with int parameter
+/// </summary>
+[System.Serializable] public class UnityIntEvent : UnityEvent<int> { }
